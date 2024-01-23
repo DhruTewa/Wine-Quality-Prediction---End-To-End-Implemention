fab466e7df6d7e75eb34790b4cbdab295b9f3008
Dhruv Tewari <dhrutewa@gmail.com>
2024-01-22 21:02:40 -0600

    Jupyter Notebook Added

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Wine-Quality-Prediction-End-To-End-Implemention
+
 The idea behind for this project is not to actually predict the outcome but to show how any projects can be implemented end to end. What are the steps should be taken and what all things we should take care of while working on any Machine Learning Project.
+
+## To create a virtual enviornment
+
+ - conda create -n mlproj python=3.8 -y
+ - conda activate mlproj
+
+
--- template.py
+++ template.py
@@ -28,16 +28,21 @@ list_of_files = [
     "templates/index.html"
     ]
 for filepath in list_of_files:
-    path = Path(filepath)
-    filedir,filename = os.path.split(filepath)
-    if filedir != "":
-       os.makedirs(filedir,exist_ok=True)
-       logging.info(f"Creating directory; {filedir} for the file : {filename}")
+    filepath = Path(filepath)
+
+    filedir, filename = os.path.split(filepath)
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
-        with open(filepath,"w") as f:
+        with open(filepath, "w") as f:
             pass
-            logging.info(f"Creating empty file : {filepath}")
+            logging.info(f"Creating empty file: {filepath}")
+
+
     else:
-        logging.info(f"{filename} already exists")
+        logging.info(f"{filename} is already exists")
     
         
